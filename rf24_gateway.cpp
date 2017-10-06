059e90fb3cf31a7b828ba009f217f56d9f3eec98
xerano <mlehmann83@gmail.com>
2017-10-06 08:12:49 +0200

    removed duplicate

--- rf24_gateway.cpp
+++ rf24_gateway.cpp
@@ -14,10 +14,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <bcm2835.h>
-<<<<<<< HEAD
 #include <signal.h>
-=======
->>>>>>> 55929dc2704780ae46d135b8e432132ba16c8fef
 
 #define LED_PIN RPI_GPIO_P1_11
 #define PIPES 3
@@ -25,20 +22,12 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-struct TinyTemp {
-=======
 typedef struct TinyTemp {
->>>>>>> 55929dc2704780ae46d135b8e432132ba16c8fef
     float temp;
     int vcc;
 };
 
-<<<<<<< HEAD
-struct TinyTemp tinyTemp;
-=======
 TinyTemp tinyTemp;
->>>>>>> 55929dc2704780ae46d135b8e432132ba16c8fef
 
 RF24 radio(22,0);
 struct mosquitto *mosq;
@@ -47,20 +36,16 @@ void eventLoop();
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint8_t pipes[][6] = {"1Node","2Node", "3Node"};
-<<<<<<< HEAD
 
 void init_mosquitto(){
     mosq = mosquitto_new("id", true, NULL);
  	mosquitto_connect(mosq, "localhost", 1883, 0);
-     int loop = mosquitto_loop_start(mosq);
+	int loop = mosquitto_loop_start(mosq);
     if(loop != MOSQ_ERR_SUCCESS){
         fprintf(stderr, "Unable to start loop: %i\n", loop);
         exit(1);
     }
 }
-
-void setup_radio(){
-=======
 
 volatile char should_disconnect = 0;
 
@@ -72,21 +57,14 @@ void on_publish(struct mosquitto *mosq, void *userdata, int mid)
 	}
 }
 
-void init_mosquitto(){
-    mosq = mosquitto_new("id", true, NULL);
-    mosquitto_publish_callback_set(mosq, on_publish);
-}
-
 void setup_radio(){
 	// Setup and configure rf radio
->>>>>>> 55929dc2704780ae46d135b8e432132ba16c8fef
     radio.begin();
     radio.setRetries(15,15);
     radio.setDataRate(RF24_250KBPS);
     radio.printDetails();
     radio.openReadingPipe(1,pipes[1]);
     radio.openReadingPipe(2,pipes[2]);
-<<<<<<< HEAD
     radio.startListening();
 }
 
@@ -95,7 +73,7 @@ void setup_radio(){
 void sighandler(int signum){
     mosquitto_destroy(mosq);
     mosquitto_lib_cleanup();
-    fprintf(stderr, "Cleaning up mosquittoi\n");
+    fprintf(stderr, "Cleaning up mosquitto\n");
     exit(0);
 }
 
@@ -161,72 +139,4 @@ void eventLoop(){
 		}
 	  delay(100); //Delay after payload responded to, minimize RPi CPU time
 	} // forever loop
-
-=======
-      
-    radio.startListening();
-}
-
-int main(int argc, char** argv){
-	
-	printf("Setup radio...\n");
-    setup_radio();
-
-    // init LED
-    //printf("Initializing status LED...\n");
-    //bcm2835_gpio_fsel(LED_PIN, BCM2835_GPIO_FSEL_OUTP);
-    
-    printf("Initializing mosquitto...\n");
-    init_mosquitto();
-	
-    eventLoop();
-}
-
-void eventLoop(){
-	while (1)
-	{
-		// if there is data ready
-		uint8_t pipe = 1;
-
-		//bcm2835_gpio_write(LED_PIN, LOW);
-
-		if ( radio.available(&pipe) )
-		{
-			//bcm2835_gpio_write(LED_PIN, HIGH);
-
-			// Fetch the payload, and see if this was the last one.
-			while(radio.available()){
-				radio.read( &tinyTemp, sizeof(tinyTemp) );
-			}
-							
-			mosquitto_connect(mosq, "localhost", 1883, 0);
-			printf("received(%d)  temp: %f vcc: %d \n", pipe, tinyTemp.temp, tinyTemp.vcc);
-            
-            char topic_temp[50]; 
-            char topic_vcc[50];
-            
-            sprintf(topic_temp, "emon/%d/temp", pipe);
-            sprintf(topic_vcc, "emon/%d/vcc", pipe);
-            
-            char temp_val[10];
-            char vcc_val[10];
-            sprintf(temp_val, "%.3f", tinyTemp.temp);
-            sprintf(vcc_val, "%.3f", tinyTemp.vcc / 1000.0);
-            
-            printf("Temp: %s\n", temp_val);
-            printf("VCC: %s\n", vcc_val);
-
-			mosquitto_publish(mosq, NULL, topic_temp, strlen(temp_val), temp_val, 0, false);
-			mosquitto_publish(mosq, NULL, topic_vcc, strlen(vcc_val), vcc_val, 0, false);
-			should_disconnect++;
-
-            pipe++;
-			if(pipe>PIPES){
-				pipe=0;
-			}
-		}
-	  delay(100); //Delay after payload responded to, minimize RPi CPU time
-	} // forever loop
-
->>>>>>> 55929dc2704780ae46d135b8e432132ba16c8fef
 }
